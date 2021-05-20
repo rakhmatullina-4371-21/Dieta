0027360ea34264d7645da04831ddf6802257d492
DINA <DINA@DESKTOP-0V54PT2>
2021-05-20 21:08:12 +0300

    Добавьте файлы проекта.

---
+++ Diet/Areas/Patient/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diet.Areas.Patient.Contollers
+{
+    public class HomeController : Controller
+    {
+        [Area("Patient")]
+        public IActionResult Menu()
+        {
+            return View();
+        }
+    }
+}
---
+++ Diet/Contollers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diet.Contollers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Start()
+        {
+            return View();
+        }
+    }
+}
